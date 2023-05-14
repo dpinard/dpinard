d9b93365a67db8be01ea69a9d0f89a35548dd26a
test1 <test1email>
2023-05-14 11:07:20 +0200

    add font jakarta sans

--- testproject/src/App.js
+++ testproject/src/App.js
@@ -22,19 +22,25 @@ import Icon from '@mui/material/Icon';
 
 function App() {
   return (
-      <div>
-    <head>
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-    </head>
+    <div>
+      <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link href='https://fonts.googleapis.com/css?family=Plus Jakarta Sans:300,400,500' rel='stylesheet'/>
+
+      </head>
+    
     <Box sx={{ display: 'flex' }}>
+      
       <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>        
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
             Damien Pinard
           </Typography>
         </Toolbar>
       </AppBar>
+
       <Drawer
         variant="permanent"
         sx={{
@@ -42,54 +48,40 @@ function App() {
           width: 240,
           flexShrink: 0,
           [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
-        }}
-      >
+        }}>
       </Drawer>
+
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        
         <Toolbar />
-      <Box
-        sx={{
-          display:'flex',
-          flexWrap: 'wrap',
-          '&> :not(style)': {
-            p:3,
-            width: 256,
-            height: 128,
-          },
-        }}
-      >
-      <Paper sx={{m:4,p:4}}>
-        
-        <Box sx={{display:'flex',justifyContent:'space-between'}}>
-
-          <Box sx={{display:'flex', flexDirection:'column', }}>          
-            <Typography variant='h6' textTransform='uppercase' fontFamily="Arial" letterSpacing='0.5px' fontWeight='600' fontSize='0.90em'> Projets </Typography>
-            <Typography variant='h5' fontFamily="sans-serif" lineHeight='1.7' fontWeight='700' fontSize='2rem'> 123 </Typography>
-          </Box>
-        
-          <Avatar sx={{p:3, backgroundColor:'green'}}>
-            <TopicOutlinedIcon/>
-          </Avatar>
-
+      
+        <Box
+          sx={{
+            display:'flex',
+            flexWrap: 'wrap',
+            '&> :not(style)': {
+              p:3,
+              width: 256,
+              height: 128,
+            },
+          }}>
+  
+          <Paper sx={{m:4,p:4}}>  
+            <Box sx={{display:'flex',justifyContent:'space-between'}}>
+              <Box sx={{display:'flex', flexDirection:'column', }}>          
+                <Typography variant='h6' textTransform='uppercase' fontFamily="Arial" letterSpacing='0.5px' fontWeight='600' fontSize='0.90em'> Projets </Typography>
+                <Typography variant='h5' fontFamily="Plus Jakarta Sans, sans-serif" lineHeight='1.7' fontWeight='700' fontSize='2rem'> 123 </Typography>
+              </Box>
+              <Avatar sx={{p:3, backgroundColor:'green'}}>
+                <TopicOutlinedIcon/>
+              </Avatar>
+            </Box>
+          </Paper>
         </Box>
-
-      </Paper>
-
-
-    </Box>
         
 
       </Box>
     </Box>
-
-
-
-
-
-
-
-
-
 
     </div>
   );
