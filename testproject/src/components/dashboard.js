19bda73502095e2749f5972bfbbe80190be28c12
dpinard <57499432+dpinard@users.noreply.github.com>
2023-05-24 05:32:06 +0200

    Update dashboard.js

--- testproject/src/components/dashboard.js
+++ testproject/src/components/dashboard.js
@@ -41,7 +41,7 @@ export default function Dashboard () {
                     <Box sx={{display:'flex',justifyContent:'space-between'}}>
                     <Box sx={{display:'flex', flexDirection:'column', }}>          
                         <Typography variant='h6' textTransform='uppercase' fontFamily="Arial" letterSpacing='0.5px' fontWeight='600' fontSize='0.90em'> Capital </Typography>
-                        <Typography variant='h5' fontFamily="Plus Jakarta Sans, sans-serif" lineHeight='1.7' fontWeight='700' fontSize='2rem'> 50$ </Typography>
+                        <Typography variant='h5' fontFamily="Plus Jakarta Sans, sans-serif" lineHeight='1.7' fontWeight='700' fontSize='2rem'> 50€ </Typography>
                     </Box>
                     <Avatar sx={{p:3, backgroundColor:'green'}}>
                         <SavingsIcon/>
