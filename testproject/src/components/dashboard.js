94a8c66cc2c68a04c81acd70f388ebd9dbca3171
dpinard <57499432+dpinard@users.noreply.github.com>
2023-05-24 05:31:08 +0200

    Update dashboard.js card

--- testproject/src/components/dashboard.js
+++ testproject/src/components/dashboard.js
@@ -40,7 +40,7 @@ export default function Dashboard () {
                 <Paper sx={styles.dashCardPaper}>  
                     <Box sx={{display:'flex',justifyContent:'space-between'}}>
                     <Box sx={{display:'flex', flexDirection:'column', }}>          
-                        <Typography variant='h6' textTransform='uppercase' fontFamily="Arial" letterSpacing='0.5px' fontWeight='600' fontSize='0.90em'> Budget </Typography>
+                        <Typography variant='h6' textTransform='uppercase' fontFamily="Arial" letterSpacing='0.5px' fontWeight='600' fontSize='0.90em'> Capital </Typography>
                         <Typography variant='h5' fontFamily="Plus Jakarta Sans, sans-serif" lineHeight='1.7' fontWeight='700' fontSize='2rem'> 50$ </Typography>
                     </Box>
                     <Avatar sx={{p:3, backgroundColor:'green'}}>
