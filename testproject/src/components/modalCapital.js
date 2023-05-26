ff51c5eb3d455fe29c9d18b7de223328d8e40917
test1 <test1email>
2023-05-26 18:07:30 +0200

    css add modal capital

--- testproject/src/components/modalCapital.js
+++ testproject/src/components/modalCapital.js
@@ -9,7 +9,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 500,
+  width: '40%',
   bgcolor: 'background.paper',
 //   border: '1px solid #000',
   boxShadow: 24,
