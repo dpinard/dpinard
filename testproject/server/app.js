aad8b8c1e6e1c96d735a9285c80080d3d8cb258a
test1 <test1email>
2023-06-30 11:51:30 +0200

    struct md-signup user save

--- testproject/server/app.js
+++ testproject/server/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8000
 const bodyParser = require('body-parser');
-
+const md_hashPwd = require('./src/middleware/md-signup');
+const User = require('./src/schema.js');
 
 const mongoose = require('mongoose');
 
@@ -12,13 +13,7 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-  
-const userSchema = new mongoose.Schema({
-  name: { type: String, unique: true,},
-  hashPwd: { type: String, }
 
-});
-const User = mongoose.model('user', userSchema);
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
@@ -30,18 +25,14 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/signup', (req, res) => {
-    const user = new User({name: 'mat'})
 
-    res.send(req.body);
-
-    // user.save()
-      // .then(() => {
-        // res.status(200).send('utilisateur sign up');
-      // })
-      // .catch((error) => {
-        // res.send(error);
-      // })
 })
+
+app.post('/signup', md_hashPwd, (req, res) => {
+  res.send({msg: req.body.msg});
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
