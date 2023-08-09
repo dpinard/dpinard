d294c787bb5c4e9fa41c79eaa773aefecd87639f
dam <operateur.am0@gmail.com>
2023-08-09 13:34:35 +0200

    ft_find

---
+++ testproject/server/src/libft/ft_find.js
@@ -0,0 +1,12 @@
+
+const friends = [
+    [name = 'test0', id = 'aaabbbccc'],
+    [name = 'd4m', id = 'a256254asdcx'],
+    [name = '7iNd4', id = 'sqsqddffff'],
+]
+
+function ft_find(params) {
+
+}
+
+console.log(friends)
